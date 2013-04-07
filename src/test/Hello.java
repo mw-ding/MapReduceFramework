215f9f387d3b3964ef5151732ee659e38faa479b
Mengwei Ding <mengwei.ding@gmail.com>
2013-04-07 15:03:17 -0400

    rmi basic

---
+++ src/test/Hello.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Hello extends UnicastRemoteObject implements HelloInterface {
+
+	private String message;
+	
+	public Hello(String msg) throws RemoteException {
+		this.message = msg;
+	}
+	
+	public String say() throws RemoteException {
+		return this.message;
+	}
+	
+}
