aeeef7170bb1e6b90dcce3bd545aec634e7f1840
Mengwei Ding <mengwei.ding@gmail.com>
2013-04-07 18:04:34 -0400

    job client related

--- src/JobTrackerServices.java
+++ src/JobTrackerServices.java
@@ -1,7 +1,7 @@
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-public class JobTrackerServices extends UnicastRemoteObject implements StatusUpdater{
+public class JobTrackerServices extends UnicastRemoteObject implements StatusUpdater, JobSubmitter {
 
 	private JobTracker jobTracker;
 	
@@ -12,6 +12,16 @@ public class JobTrackerServices extends UnicastRemoteObject implements StatusUpd
 
 	@Override
 	public void update(Object statuspck) throws RemoteException {
+		
+	}
+
+	@Override
+	public int getJobID() throws RemoteException {
+		return 0;
+	}
+
+	@Override
+	public void submitJob(Object job) throws RemoteException {
 		
 	}
 
