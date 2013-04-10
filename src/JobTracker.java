4b64b921fbf2072d7a2896be832ab70fff2bc8d4
Mengwei Ding <mengwei.ding@gmail.com>
2013-04-10 01:28:26 -0400

    commit

--- src/JobTracker.java
+++ src/JobTracker.java
@@ -12,7 +12,7 @@ import java.util.jar.JarFile;
 
 public class JobTracker {
 	
-	public final static String JOBTRACKER_SERVICE_NAME = "JobTrackerService";
+	public final static String JOBTRACKER_SERVICE_NAME = "job_tracker_service";
 
 	// all the tasktrackers running under current system
 	private Map<String, TaskTrackerMeta> tasktrackers;
@@ -66,7 +66,10 @@ public class JobTracker {
 		this.reduceTasks = Collections.synchronizedMap(new HashMap<Integer, TaskMeta>());
 		this.jobs = Collections.synchronizedMap(new HashMap<Integer, JobMeta>());
 		
-		this.mapTasksQueue = (Queue<TaskMeta>) Collections.synchronizedCollection(new PriorityQueue<TaskMeta>(10, new Comparator<TaskMeta>() {
+		TaskTrackerMeta m = new TaskTrackerMeta("tt1", null);
+		this.tasktrackers.put("tt1", m);
+		
+		this.mapTasksQueue = (Queue<TaskMeta>) (new PriorityQueue<TaskMeta>(10, new Comparator<TaskMeta>() {
 
 			@Override
 			public int compare(TaskMeta o1, TaskMeta o2) {
@@ -75,7 +78,7 @@ public class JobTracker {
 			
 		}));
 		
-		this.reduceTasksQueue = (Queue<TaskMeta>) Collections.synchronizedCollection(new PriorityQueue<TaskMeta>(10, new Comparator<TaskMeta>() {
+		this.reduceTasksQueue = (Queue<TaskMeta>) (new PriorityQueue<TaskMeta>(10, new Comparator<TaskMeta>() {
 
 			@Override
 			public int compare(TaskMeta o1, TaskMeta o2) {
